1e81613020550f9633fb6a471fa8c34ded415fb8
U-CENTRIFY\nate.yocom <nate.yocom@nates-w540.centrify.com>
2016-02-04 14:24:25 -0800

    Add license to all source files
    Add query and lock/unlock examples
    
    Remove last of old code
    
    Update readme

--- Centrify.Samples.DotNet.ApiLib/ApiClient.cs
+++ Centrify.Samples.DotNet.ApiLib/ApiClient.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/**
+ * Copyright 2016 Centrify Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +30,61 @@ namespace Centrify.Samples.DotNet.ApiLib
         {
             m_restClient = authenticatedClient;
         }
+
+        // Illustrates locking a CUS user via /cdirectoryservice/setuserstate
+        public void LockUser(string userUuid)
+        {
+            Dictionary<string, dynamic> args = new Dictionary<string, dynamic>();
+            args["ID"] = userUuid;            
+            args["state"] = "Locked";
+
+            var result = m_restClient.CallApi("/cdirectoryservice/setuserstate", args);
+            if (result["success"] != true)
+            {
+                Console.WriteLine("LockUser {0} failed: {1}", userUuid, result["Message"]);
+                throw new ApplicationException(result["Message"]);
+            }
+        }
+
+        // Illustrates unlocking a CUS user via /cdirectoryservice/setuserstate
+        public void UnlockUser(string userUuid)
+        {
+            Dictionary<string, dynamic> args = new Dictionary<string, dynamic>();
+            args["ID"] = userUuid;
+            args["state"] = "None";
+
+            var result = m_restClient.CallApi("/cdirectoryservice/setuserstate", args);
+            if (result["success"] != true)
+            {
+                Console.WriteLine("UnlockUser {0} failed: {1}", userUuid, result["Message"]);
+                throw new ApplicationException(result["Message"]);
+            }
+        }
+
+        // Illustrates usage of /redrock/query to run queries
+        public dynamic Query(string sql)
+        {
+            Dictionary<string, dynamic> args = new Dictionary<string, dynamic>();
+            args["Script"] = sql;
+
+            Dictionary<string, dynamic> queryArgs = new Dictionary<string, dynamic>();
+            args["Args"] = queryArgs;
+
+            queryArgs["PageNumber"] = 1;
+            queryArgs["PageSize"] = 10000;
+            queryArgs["Limit"] = 10000;
+            queryArgs["Caching"] = -1;
+
+            var result = m_restClient.CallApi("/redrock/query", args);
+            if (result["success"] != true)
+            {
+                Console.WriteLine("Running query failed: {0}", result["Message"]);
+                throw new ApplicationException(result["Message"]);
+            }
+
+            return result["Result"];
+        }
+
 
         // Illustrates usage of /saasManage/updateapplicationde to stash a new username/password for a UP application
         public void UpdateApplicationDE(string appKey, string username, string password)
@@ -36,7 +107,7 @@ namespace Centrify.Samples.DotNet.ApiLib
         public dynamic GetUPData()
         {
             Dictionary<string, dynamic> args = new Dictionary<string, dynamic>();
-            args["force"] = true;
+            args["force"] = false;
 
             var result = m_restClient.CallApi("/uprest/getupdata", args);
             if (result["success"] != true)
@@ -64,8 +135,8 @@ namespace Centrify.Samples.DotNet.ApiLib
         }
 
         // Illustrates usage of /cdirectoryservice/createuser to create a new CUS user, presumes
-        //  username and mail are the same.
-        public void CreateUser(string username, string password)
+        //  username and mail are the same.  Return value is user's UUID
+        public string CreateUser(string username, string password)
         {
             Dictionary<string, dynamic> createUserArgs = new Dictionary<string, dynamic>();
             createUserArgs["Name"] = username;
@@ -81,6 +152,7 @@ namespace Centrify.Samples.DotNet.ApiLib
             }
 
             Console.WriteLine("Creating user {0} succeeded.", username);
+            return result["Result"];
         }
 
         // Illustrates logout
--- Centrify.Samples.DotNet.Client/Program.cs
+++ Centrify.Samples.DotNet.Client/Program.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/**
+ * Copyright 2016 Centrify Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+using System;
 
 using Centrify.Samples.DotNet.ApiLib;
 
@@ -19,31 +34,68 @@ namespace Centrify.Samples.DotNet.Client
 
             try
             {
-                // Get my applications and list them out
-                var myApplications = apiClient.GetUPData();
-                Console.WriteLine("User's Application List:");
-                foreach (var app in myApplications)
-                {
-                    Console.WriteLine("Name: {0} Key: {1} Icon: {2}", app["DisplayName"], app["AppKey"], app["Icon"]);
-                }
+                #region Execute a query and display the results
+                //// Lets query for top user logins from last 30 days
+                //var queryResult = apiClient.Query(@"select NormalizedUser as User, Count(*) as Count from Event
+                //                                        where EventType = 'Cloud.Core.Login'
+                //                                            and WhenOccurred >= DateFunc('now', '-30')
+                //                                            group by User
+                //                                        order by count desc");
 
-                // Get applications assigned to the sysadmin role only
-                var sysadminApps = apiClient.GetRoleApps("sysadmin");
-                Console.WriteLine("App ID's assigned to sysadmin:");
-                foreach (var app in sysadminApps)
-                {
-                    Console.WriteLine("Key: {0}", app["Row"]["ID"]);
-                }
+                //Console.WriteLine("Query resulted in {0} results, first 5:", queryResult["FullCount"]);
 
-                // Create a new CUS user
-                // apiClient.CreateUser("joe@suffix.com", "newP@3651awdF@!%^");
+                //// Dump to console the first 5
+                //var queryResultRows = queryResult["Results"];
+                //var queryResultColumns = queryResult["Columns"];
+                //for(int x = 0; x < Math.Min(5, queryResult["FullCount"]); x++)
+                //{
+                //    Console.Write("\t");
+                //    foreach(var column in queryResultColumns)
+                //    {
+                //        Console.Write("{0} => {1}\t", column["Name"], queryResultRows[x]["Row"][column["Name"]]);
+                //    }
+                //    Console.WriteLine();
+                //}
+                #endregion
 
+                #region Get user's assigned applications
+                //// Get my applications and list them out
+                //var myApplications = apiClient.GetUPData();
+                //Console.WriteLine("User's Application List:");
+                //foreach (var app in myApplications)
+                //{
+                //    Console.WriteLine("Name: {0} Key: {1} Icon: {2}", app["DisplayName"], app["AppKey"], app["Icon"]);
+                //}
+                #endregion
+
+                #region Get applications list assigned to a role
+                //// Get applications assigned to the sysadmin role only
+                //var sysadminApps = apiClient.GetRoleApps("sysadmin");
+                //Console.WriteLine("App ID's assigned to sysadmin:");
+                //foreach (var app in sysadminApps)
+                //{
+                //    Console.WriteLine("Key: {0}", app["Row"]["ID"]);
+                //}
+                #endregion
+
+                #region Create a new CUS user
+                //// Create a new CUS user
+                // string userUuid = apiClient.CreateUser("apitest@contoso", "newP@3651awdF@!%^");
+                #endregion
+
+                #region Lock/Unlock a CUS user
+                //// Lock a CUS user's account, UUID can come from CreateUser, or a Query on DsUser table
+                //apiClient.LockUser(userUuid);
+
+                //apiClient.UnlockUser(userUuid);
+                #endregion
+
+                #region Update a username/password applications stashed credentials
                 // Update the credentials for my UP app...
                 // apiClient.UpdateApplicationDE("someAppKeyFromGetUPData", "newUsername", "newPassword");
-
-
+                #endregion
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine("Error: {0}", e);
             }
